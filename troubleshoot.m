be4e4a45e5ef268dde279634bb48b1b3bf8c6a4a
DiegoHerrera262 <diegoherrera262@gmail.com>
2020-05-13 07:42:17 -0500

    Demo of MACE Filter

--- troubleshoot.m
+++ troubleshoot.m
@@ -26,11 +26,33 @@ Filter = load('filters/Diego_filter.mat');
 Filter = Filter.filter;
 
 %% Load Sample Image
-sample = rgb2gray(imread('Diego/sample50.png'));
+% Read scene image from webcam
+cam = webcam(2);
+scene = snapshot(cam);
+scsize = size(rgb2gray(scene));
+% Take & Crop image for template matching
+x0 = int16(scsize(2)/2) - int16(orgsize(2)/2);
+y0 = 0;
+rect = [x0, y0, orgsize(2), orgsize(1)];
+%% Show Image for scene capture
+textPos = [3*scsize(2)/8 6*scsize(2)/8];
+t = 0;
+tic
+while t <= 20
+    t = toc;
+    text = num2str(t,'%0.2f');
+    scene = snapshot(cam);
+    scene = insertShape(scene,'Rectangle',rect,'LineWidth',5);
+    scene = insertText(scene,textPos,text);
+    imshow(scene);
+end
+
+%% Crop image
+sample = imresize(rgb2gray(imcrop(scene,rect)),orgsize);
 fft_sample = fft2(sample);
 
 %% Compute correlation
-fft_cor = sqrt(conj(fft_sample) .* fft_sample);
+fft_cor = sqrt(conj(fft2(img)) .* fft_sample);
 cor = abs(fftshift(ifft2(fft_cor)));
 fft_cor1 = sqrt(conj(M) .* fft_sample);
 cor1 = abs(fftshift(ifft2(fft_cor1)));
@@ -40,15 +62,18 @@ cor2 = abs(fftshift(ifft2(fft_cor2)));
 t = toc;
 
 %% Display correlation, sample image  & filter
-subplot(2,2,1);
+subplot(2,3,1);
+imshow(sample,[]);
+title('Test Image')
+subplot(2,3,3);
 imshow(img,[]);
-title('Reference Image')
-subplot(2,2,2);
+title('Sample Image')
+subplot(2,3,4);
 surf(cor)
-title('Selfcorrelation')
-subplot(2,2,3);
+title('Crosscorrelation')
+subplot(2,3,5);
 surf(cor1);
 title('Correlation Output - With 1 Smp. MACE')
-subplot(2,2,4);
+subplot(2,3,6);
 surf(cor2);
 title('Correlation Output - With Full MACE')
