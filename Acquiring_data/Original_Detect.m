2e646d44b2e540a25e0ab16d2659801f5c2a32ec
Nicolás Ávila <NicolasAlejandro@Nicolass-MacBook-Pro.local>
2020-05-23 05:31:05 -0500

    Some detail of face recongnition code

--- Acquiring_data/Original_Detect.m
+++ Acquiring_data/Original_Detect.m
@@ -1,7 +1,6 @@
 %The original code, detects faces in a frame of the video 'visionface.avi'
 %and then shows the image with a rectangle surronding the detected faces.
-%To make it work on an image, replace, on the line 10,
-%'VideoReader('visionface.avi')' by 'imread('imagename.png')'.
+%A modification wich works with an image 'imagename.png' is commented at the end.
 
 % Create a cascade detector object.
 faceDetector = vision.CascadeObjectDetector();
@@ -14,3 +13,17 @@ bbox            = step(faceDetector, videoFrame);
 % Draw the returned bounding box around the detected face.
 videoOut = insertObjectAnnotation(videoFrame,'rectangle',bbox,'Face');
 figure, imshow(videoOut), title('Detected face');
+
+---------------------------------------------------------------------------
+
+%faceDetector = vision.CascadeObjectDetector();
+
+
+%videoFrame      = imread('imagename.png');
+%bbox            = step(faceDetector, videoFrame);
+
+
+%videoOut = insertObjectAnnotation(videoFrame,'rectangle',bbox,'Face');
+%figure, imshow(videoOut), title('Detected face');
+
+--------------------------------------------------------------------------
