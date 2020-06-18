59c47e5a58c7891416e182e1863871d235f641b6
DiegoHerrera262 <diegoherrera262@gmail.com>
2020-06-18 17:14:43 -0500

    New workflow of Project

--- show_filter.m
+++
@@ -1,8 +0,0 @@
-function show_filter(filter_name)
-    dir = [pwd() '/filters/' filter_name '_filter.mat'];
-    filter = load(dir); filter = filter.filter;
-    filter = abs(filter);
-    filter = uint8(255*(filter - min(min(filter(:))))/(...
-        max(max(filter(:))) - min(min(filter(:)))));
-    imshow(filter,[]);
-end
